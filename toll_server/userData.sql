25282131230ead90b0a72282616d3e84c00400b2
rajeevbrahma <rajeevbrahma@hotmail.com>
2016-10-24 11:02:26 +0530

    userDatasql

---
+++ toll_server/userData.sql
@@ -0,0 +1,3 @@
+--userData (UserName, vehicleregistration number, vehicle type,wallet balance,blockstatus,rfid number)
+INSERT INTO VEHICLE_INFO
+VALUES ('JOHN','KA0H1234','CAR',100,0,'090096B43E15');
